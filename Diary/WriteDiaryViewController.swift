3d49586eb16428d017fd5defade315ccdf1dbd3b
gdking137 <gdkim137@gmail.com>
2022-12-09 18:00:12 -0500

    Touched basic UI and added additional screen for showing the diary

---
+++ Diary/WriteDiaryViewController.swift
@@ -0,0 +1,22 @@
+//
+//  WriteDiaryViewController.swift
+//  Diary
+//
+//  Created by Songhee Yim on 12/9/22.
+//
+
+import UIKit
+
+class WriteDiaryViewController: UIViewController {
+    
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+
+}
