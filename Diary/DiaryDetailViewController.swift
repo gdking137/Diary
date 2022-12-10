ed764076e070a947b78612e3ac3ffa3d21c95117
gdking137 <gdkim137@gmail.com>
2022-12-09 23:36:02 -0500

    added basic UI for the diary screen. The screen includes the title, date, and description about what users can add into the system

---
+++ Diary/DiaryDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DiaryDetailViewController.swift
+//  Diary
+//
+//  Created by Songhee Yim on 12/9/22.
+//
+
+import UIKit
+
+class DiaryDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
