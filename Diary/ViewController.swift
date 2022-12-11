6fc11b4bbabf75fda8ed29b3e881e4c264b91ac4
gdking137 <gdkim137@gmail.com>
2022-12-10 23:35:39 -0500

    Show added contents in the other screen using codes

--- Diary/ViewController.swift
+++ Diary/ViewController.swift
@@ -21,16 +21,49 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    private func configureCollectionView(){
+        self.collectionView.collectionViewLayout = UICollectionViewFlowLayout()
+        self.collectionView.contentInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+    }
+    
+    
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let writeDiaryViewController = segue.destination as? WriteDiaryViewController{
             writeDiaryViewController.delegate = self
         }
     }
 
+    private func dateToString(date: Date) -> String{
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yy년 MM월 dd일(EEEEE)"
+        formatter.locale = Locale(identifier: "ko_KR")
+        return formatter.string(from: date)
+    }
 }
+
+extension ViewController: UICollectionViewDataSource{
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return self.diaryList.count
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "DiaryCell", for: indexPath) as? DiaryCell else { return UICollectionViewCell()}
+        let diary = self.diaryList[indexPath.row]
+        cell.titleLabel.text = diary.title
+        cell.dateLabel.text = self.dateToString(date: diary.date)
+        return cell
+    }
+    
+}
+
+
 
 extension ViewController: WriteDiaryViewDelegate{
     func didSelectRegister(diary: Diary) {
         self.diaryList.append(diary)
     }
 }
+
+
