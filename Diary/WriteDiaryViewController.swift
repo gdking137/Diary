e3dd7fd7caebb0bf02c8440ceda4c2fed9718853
gdking137 <gdkim137@gmail.com>
2022-12-10 22:51:22 -0500

    added date picker + korean option to it

--- Diary/WriteDiaryViewController.swift
+++ Diary/WriteDiaryViewController.swift
@@ -20,10 +20,13 @@ class WriteDiaryViewController: UIViewController {
     
     @IBOutlet var confirmButton: UIBarButtonItem!
     
+    private let datePicker = UIDatePicker()
+    private var diaryDate : Date?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.configureContentsTextview()
+        self.configureContentsTextview() //make description bar appear
+        self.configureDatePicker()
         
     }
 
@@ -34,7 +37,23 @@ class WriteDiaryViewController: UIViewController {
         self.contentsTextView.layer.cornerRadius = 5.0
     }
 
-    @IBAction func tapConfirmButton(_ sender: UIBarButtonItem) {
+    private func configureDatePicker(){
+        self.datePicker.datePickerMode = .date
+        self.datePicker.preferredDatePickerStyle = .wheels
+        self.datePicker.addTarget(self, action: #selector(datePickerValueDidChange(_:)), for: .valueChanged)
+        self.datePicker.locale = Locale(identifier: "ko-KR")
+        self.dateTextField.inputView = self.datePicker
     }
     
+    @IBAction func tapConfirmButton(_ sender: UIBarButtonItem) {
+    }
+ 
+    
+    @objc private func datePickerValueDidChange(_ datePicker: UIDatePicker){
+        let formmater = DateFormatter()
+        formmater.dateFormat = "yyyy/MM/dd(EEEEE)"
+        formmater.locale = Locale(identifier: "ko_KR")
+        self.diaryDate = datePicker.date
+        self.dateTextField.text = formmater.string(from: datePicker.date)
+    }
 }
