429c27d98b76e6640f9d3e5ceb24be6a5e9f1c9b
gdking137 <gdkim137@gmail.com>
2022-12-11 22:49:35 -0500

    touching the diary now brings its information(title, description, and date) and others(buttons)

--- Diary/DiaryDetailViewController.swift
+++ Diary/DiaryDetailViewController.swift
@@ -9,10 +9,38 @@ import UIKit
 
 class DiaryDetailViewController: UIViewController {
 
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var contentsTextView: UITextView!
+    @IBOutlet var titleLabel: UILabel!
+    
+    
+    var diary: Diary?
+    var indexPath: IndexPath?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.configureView()
     }
     
-
+    private func configureView(){
+        guard let diary = self.diary else {return}
+        self.titleLabel.text = diary.title
+        self.contentsTextView.text = diary.contents
+        self.dateLabel.text = self.dateToString(date: diary.date)
+    }
+    
+    private func dateToString(date: Date) -> String{
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yy년 MM월 dd일(EEEEE)"
+        formatter.locale = Locale(identifier: "ko_KR")
+        return formatter.string(from: date)
+    }
+    
+    
+    
+    @IBAction func tapEditButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func tapDeleteButton(_ sender: UIButton) {
+    }
 }
